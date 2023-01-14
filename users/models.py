61a4f473917365df02ba7f5d53bec1e7bf4adde5
Hanane-fallah <hananee.fallah@gmail.com>
2023-01-14 15:31:16 +0330

    002 - add primarykey to users

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Users(models.Model):
-    username = models.CharField(max_length=100)
+    username = models.CharField(max_length=100, unique=True, primary_key=True)
     email = models.EmailField()
     profile_image = models.ImageField(upload_to='/images')
 
