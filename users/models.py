086a068945aa2f78d462d1c305859d54255f3aad
Hanane-fallah <hananee.fallah@gmail.com>
2023-01-14 15:55:28 +0330

    003 - complete user model

--- users/models.py
+++ users/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Users(models.Model):
     username = models.CharField(max_length=100, unique=True, primary_key=True)
     email = models.EmailField()
-    profile_image = models.ImageField(upload_to='/images')
+    profile_image = models.ImageField(upload_to='images', default='Screenshot_from_2023-01-07_20-45-18.png')
 
