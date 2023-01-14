782762c57cb04240098c20032f4d3a2268c41bb9
Hanane-fallah <hananee.fallah@gmail.com>
2023-01-14 22:42:32 +0330

    011 - add all users view

--- task/views.py
+++ task/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from task.models import Task
+from users.models import User
+
+
 # Create your views here.
+def all_users(request):
+    users = User.objects.all()
+    tasks = Task.objects.all()
+    context = {
+        'users': users,
+        'tasks': tasks
+    }
+    return render(request, 'index.html', context)
+
