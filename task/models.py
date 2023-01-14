b28abd1a1b1ccc16e08ffe72bd9082ef91db2925
Hanane-fallah <hananee.fallah@gmail.com>
2023-01-14 16:26:10 +0330

    006 - add done field to task model

--- task/models.py
+++ task/models.py
@@ -10,7 +10,8 @@ class Task(models.Model):
     ]
     user = models.ForeignKey(Users, on_delete=models.CASCADE)
     task_type = models.CharField(max_length=3, choices=task_types, default='per')
-    title = models.CharField(max_length=120)
+    title = models.CharField(max_length=120, null=True)
+    done = models.BooleanField(default=False)
 
     def __str__(self):
         return f'{self.title} : {self.user}'
