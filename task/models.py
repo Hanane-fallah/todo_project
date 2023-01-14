dace4b2d7016067f255276abd3047d34510cb821
Hanane-fallah <hananee.fallah@gmail.com>
2023-01-14 15:56:22 +0330

    004 - start task app & add task model

---
+++ task/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from users.models import Users
+# Create your models here.
+class Task(models.Model):
+    task_types = [
+        ('edu', 'education'),
+        ('job', 'job'),
+        ('per', 'personal'),
+        ('fam', 'family')
+    ]
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    task_type = models.CharField(max_length=3, choices=task_types, default='per')
